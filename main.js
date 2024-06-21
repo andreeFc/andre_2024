14a83f659bba0a84abfa410f36f62b6e5cedbf8d
andreeFc <166128253+andreeFc@users.noreply.github.com>
2024-06-21 08:33:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,7 @@ for(let i=0;i <botoes.length;i++){
             botoes[j].classList.remove('ativo'); }
         botoes[i].classList.add('ativo');}
         }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoatual = new Date();
+contadores[0].textContent = tempoObjetivo1- tempoatual;
